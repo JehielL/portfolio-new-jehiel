7efeb76c2dbdda649da4d5000aae9664bbd0bafe
JehielL <jehiel.py@gmail.com>
2024-04-29 18:19:52 +0200

    Agregado nuevo portfolio, en angular.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsListComponent } from './projects-list/projects-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+
+    {
+        path: 'projects-list',
+        component: ProjectsListComponent
+    }
+];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import Aos from 'aos';
+import { CommonModule } from '@angular/common';
+import { ParallaxStandaloneDirective } from '@yoozly/ngx-parallax';
+import { CarouselModule } from 'primeng/carousel';
+
+
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [NgbAccordionModule, CommonModule, ParallaxStandaloneDirective, CarouselModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+
+
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    Aos.init();
+  }
+  
+  backgroundStyles: { [key: string]: string } = {}; // Define the type of backgroundStyles
+
+  constructor() { }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    requestAnimationFrame(() => {
+      this.backgroundStyles['background-position-y'] = `${window.scrollY * 0.1}px`;
+    });
+  }
+}
