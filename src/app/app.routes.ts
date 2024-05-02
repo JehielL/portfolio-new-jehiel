220a6b3b3cab2e6cb50c012dc753d747798e6113
JehielL <jehiel.py@gmail.com>
2024-05-02 21:01:37 +0200

    Agregado component blog-tech, refactor home, footer, urls .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProjectsListComponent } from './projects-list/projects-list.component';
+import { BlogTechComponent } from './blog-tech/blog-tech.component';
 
 export const routes: Routes = [
     {
@@ -11,5 +12,9 @@ export const routes: Routes = [
     {
         path: 'projects-list',
         component: ProjectsListComponent
+    },
+    {
+        path: 'blog-tech',
+        component: BlogTechComponent
     }
 ];
---
+++ src/app/blog-tech/blog-tech.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-tech',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-tech.component.html',
+  styleUrl: './blog-tech.component.css'
+})
+export class BlogTechComponent {
+
+}
